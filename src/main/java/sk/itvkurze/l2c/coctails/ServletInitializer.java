ee260044033734c72268a8f2dfbff15e5b724654
Majo P. <maj0@post.sk>
2022-01-05 15:12:55 +0100

    added ServletInitializer.java and edited pom.xml

---
+++ src/main/java/sk/itvkurze/l2c/coctails/ServletInitializer.java
@@ -0,0 +1,12 @@
+package sk.itvkurze.l2c.coctails;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CoctailsApplication.class);
+	}
+}
