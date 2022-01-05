2694805988fa0f0a8130bc56e47a4f69d0981946
Majo P. <maj0@post.sk>
2022-01-05 14:31:59 +0100

    spring initialize

---
+++ src/main/java/sk/itvkurze/l2c/coctails/CoctailsApplication.java
@@ -0,0 +1,13 @@
+package sk.itvkurze.l2c.coctails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoctailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoctailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/itvkurze/l2c/coctails/CoctailsApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.itvkurze.l2c.coctails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoctailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
