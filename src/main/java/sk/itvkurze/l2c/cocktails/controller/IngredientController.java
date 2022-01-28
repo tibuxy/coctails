a0e468bfe7b4e4dabd013c2ed1b12f8c3cdb361e
Majo P. <maj0@post.sk>
2022-01-28 17:02:26 +0100

    form model

--- src/main/java/sk/itvkurze/l2c/cocktails/controller/IngredientController.java
+++ src/main/java/sk/itvkurze/l2c/cocktails/controller/IngredientController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import sk.itvkurze.l2c.cocktails.model.IngredientsFormModel;
 import sk.itvkurze.l2c.cocktails.repository.IIngredientRepository;
 
 @Controller
@@ -17,7 +20,14 @@ public class IngredientController {
 	public String getIngredients(Model model) {
 
 		model.addAttribute("ingredients", ingredientRepository.findAll());	// tu sa pracuje s chybajucim suborom ingredients.html
+		model.addAttribute("ingredientsFormModel", new IngredientsFormModel());
 		
 		return "ingredients";  // return ingredients.html from /templates
+	}
+	
+	@PostMapping("/result")
+	public String result(@ModelAttribute IngredientsFormModel formModel, Model model) {
+		Long[] ingredientIds = formModel.getIngredientIds();
+		return "result"; // return result.html from /templates
 	}
 }
